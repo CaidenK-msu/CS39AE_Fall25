1b4d880be62e35e84804be95e2caaefe52836d8a
Caiden K. <123662615+CaidenK-msu@users.noreply.github.com>
2025-10-20 17:18:11 -0600

    Change photo path to a closer image
    Updated photo path to a closer picture and commented out the previous path.

--- streamlit_CS/pages/1_Bio.py
+++ streamlit_CS/pages/1_Bio.py
@@ -16,7 +16,9 @@ FUN_FACTS = [
     "I want to build either an app and successful website that helps people in anyway possible (lifestyle wise).",
 ]
 
-PHOTO_PATH = "streamlit_CS/assets/IMG_2159.jpg"  # Put a file in repo root or set a URL
+# PHOTO_PATH = "streamlit_CS/assets/IMG_2159.jpg"  # Further/Full-Body Picture
+
+PHOTO_PATH = "streamlit_CS/assets/IMG_2154.jpg"    #Closer Picture
 
 # ---------- Layout ----------
 col1, col2 = st.columns([1, 2], vertical_alignment="center")
